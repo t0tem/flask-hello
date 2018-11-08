51d37b9eb2fe8a3fb41c316fe83237c0d708ee61
t0tem <vadimkul@gmail.com>
2018-11-08 12:46:39 +0300

    "add mean calc"

--- hello.py
+++ hello.py
@@ -5,3 +5,20 @@ app = Flask(__name__)
 def hello_world():
     print(1+2)
     return '<h1>Hello, my very best friend!!!</h1>'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    username = float(username) * float(username)
+    return 'User %s' % username
+
+def mean(nums):
+    return float(sum(nums)) / max(len(nums), 1)
+
+@app.route('/avg/<nums>')
+def avg(nums):
+    nums = nums.split(',')
+    nums = [float(num) for num in nums]
+    nums_mean = mean(nums)
+    print(nums_mean)
+    return str(nums_mean)
