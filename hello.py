5a0634acd2517512d7e06a7fb76ce4601f1a7d56
t0tem <vadimkul@gmail.com>
2018-11-11 23:38:44 +0300

    simple knn and .pkl file

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 from flask import Flask
+import numpy as np
+from sklearn.externals import joblib
+
 app = Flask(__name__)
+
+knn = joblib.load('knn.pkl') 
 
 @app.route('/')
 def hello_world():
@@ -30,30 +35,6 @@ def iris(param):
     param = [float(i) for i in param]
 
     print(param)
-
-    import numpy as np
-    from sklearn import datasets
-    iris = datasets.load_iris()
-    iris_X = iris.data
-    iris_y = iris.target
-    np.unique(iris_y)
-
-    # Split iris data in train and test data
-    # A random permutation, to split the data randomly
-    np.random.seed(0)
-    indices = np.random.permutation(len(iris_X))
-    iris_X_train = iris_X[indices[:-10]]
-    iris_y_train = iris_y[indices[:-10]]
-    iris_X_test  = iris_X[indices[-10:]]
-    iris_y_test  = iris_y[indices[-10:]]
-    # Create and fit a nearest-neighbor classifier
-    from sklearn.neighbors import KNeighborsClassifier
-    knn = KNeighborsClassifier()
-    knn.fit(iris_X_train, iris_y_train) 
-    KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',
-    metric_params=None, n_jobs=None, n_neighbors=5, p=2,
-    weights='uniform')
-    knn.predict(iris_X_test)
 
     param = np.array(param).reshape(1,-1)
     predict = knn.predict(param)
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 docker exec -it flask-hello_flask_1 bash
+docker exec -it flask-hello_flask_1 python train_model.py
+
+
 
 https://www.youtube.com/watch?v=NXp-9Tal2p8&index=9&list=PLQJ7ptkRY-xYLEAC5Y_sKqrJ9RA-U7Dja
